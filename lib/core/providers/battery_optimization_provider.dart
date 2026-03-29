60f18f2287f5b462bb23e75aa4144a4856abe074
Dencel K Babu <dencelbabu@gmail.com>
2026-03-29 15:24:19 +0530

    fix(providers): add missing flutter_riverpod import

--- lib/core/providers/battery_optimization_provider.dart
+++ lib/core/providers/battery_optimization_provider.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:pomer/core/services/battery_optimization_service.dart';
 
--- lib/core/providers/power_management_provider.dart
+++ lib/core/providers/power_management_provider.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:pomer/core/services/power_management_service.dart';
 
